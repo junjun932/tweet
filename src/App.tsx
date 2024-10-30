9bcbea2a715d8fd70cef9ec6ca0f6056210efe06
jun <1004ji93@gmail.com>
2024-10-30 20:56:44 +0900

    page setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import Layout from "./components/layout";
+import Profile from "./routes/profile";
+
+const router = createBrowserRouter ([
+  {
+    path:"/" ,
+    element: <Layout /> ,
+    children : [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+       path: "profile",
+       element: <Profile />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <></>;
+  return (<>
+  <RouterProvider router ={router} />
+  </>
+  );
 }
 
 export default App;
