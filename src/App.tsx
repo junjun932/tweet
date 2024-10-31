af4588dfcb888bb1b5f3ac5ca88c298c09fdea2e
jun <1004ji93@gmail.com>
2024-10-31 10:20:46 +0900

    feat page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Profile from "./routes/profile";
 
 const router = createBrowserRouter ([
   {
-    path:"/" ,
+    path: "/",
     element: <Layout />,
     children: [
       {
@@ -14,12 +14,32 @@ const router = createBrowserRouter ([
         element: <Home />,
       },
       {
-       path: "profile",
-       element: <Profile />,
+        path: "profile",
+        element: <Profile />,
       },
     ],
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
 ]);
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
 
 function App() {
   const [isLoading, setIsLoading] =useState(true);
