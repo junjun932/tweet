1333f5693aff3f21bd74c4e715361cd02492f244
jun <1004ji93@gmail.com>
2024-11-05 18:17:41 +0900

    post setting

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,16 +1,78 @@
+import { Styled } from "styled-components";
 
-const Form = styled.form``;
+const Form = styled.form`
+display:flex;
+flex-direction: column;
+gap: 10px;
+`;
 
-const TextArea = styled.textarea``;
 
-const AttachFileButton = styled.label``;
 
-const AttachFileInput = styled.input``;
+const TextArea = styled.textarea`
+border: 2px solid white;
+padding: 20px;
+border-radius: 20px;
+font-size: 16px;
+color:white;
+background-color: black;
+resize: none;
+font-family: Georgia, 'Times New Roman', Times, serif;
+&: :placeholder {
+    font-size: 16px;
+}
+&:focus {
+    outline: none;
+    border-color: #1d9bf0;
 
-const SubmitBtn = styled.input``;
+}
+`;
+
+const AttachFileButton = styled.label`
+padding: 10px 0px;
+color:#1d9bf0;
+text-align: center;
+border-radius: 20px;
+border: 1px solid #1d9bf0;
+font-size: 14px;
+font-weight: 600;
+cursor: pointer;
+`;
+
+const AttachFileInput = styled.input`
+display:none;
+`;
+
+const SubmitBtn = styled.input`
+background-color: #1d9bf0;
+color:white;
+border: none;
+padding: 10px 0px;
+border-radius: 20px;
+font-size: 16px;
+cursor: pointer;
+&:hover,
+&:active {
+    opacity: 0.9;
+}
+`;
 
 
 export default function PostTweetForm(){
+const [isLoading, setLoading]=useState(false);
+const [tweet, setTweet] =useState("");
+const[file, setFile]=useState<File |null>(null);
+const onChange =( e:React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+};
+const onFileChange =(e:React.ChangeEvent<HTMLInputElement>)=> {
+    const {files} =e.target;
+    if(files &&files.length ===1) {
+        setFile(files[0]);
+    }
+};
+
+
+
     return <Form>
         <TextArea placeholder= "wth is going on?!" />
         <AttachFileButton htmlFor= "file">Add Photo</AttachFileButton>
